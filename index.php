3580d49555aa44a52c73298bf0390d4b8308e7ef
sfyanbel <sfyanbelhag7@gmail.com>
2023-02-19 00:43:48 +0200

    OOP

--- index.php
+++ index.php
@@ -1,27 +1,31 @@
 <?php
-
-class AmericanExpress
+abstract class CreditCard
+{
+    private  $FNumber;
+    private  $name;
+}
+class AmericanExpress extends CreditCard
 {
     //American Express cards always begin with the number 3, more specifically 34 or 37
     static $FNumber=3;
 
     static $name = 'This is AmericanExpress Credit Card';
 }
-class Visa 
+class Visa extends CreditCard
 {
     //Visa cards begin with the number 4.
     static $FNumber=4;
  
     static $name = 'This is Visa Credit Card';
 }
-class MasterCards
+class MasterCards extends CreditCard
 {
     //Mastercards start with the number 5.
     static $FNumber=5;
   
     static $name = 'This is MasterCards Credit Card';
 }
-class Discover
+class Discover extends CreditCard
 {
     //Discover Cards begin with the number 6.
     static $FNumber=6;
@@ -29,13 +33,11 @@ class Discover
     static $name = 'This is Discover Credit Card';
 }
 
-
 // Test Credit Card
-$testCreditCard = "494341960148925";
+$testCreditCard = "594341960148925";
 
 //first number of test Credit Card
 $firstNumber = substr($testCreditCard, 0, 1);
-
 
 // index = First Number of the every Credit Type &&
 // value = name of the Credit Card
@@ -44,16 +46,11 @@ $CreditCard = [
     Visa::$FNumber => Visa::$name,
     MasterCards::$FNumber => MasterCards::$name,
     Discover::$FNumber => Discover::$name,
-    // as $firstNumber just one char is possible to Control to the safty null index 
-    //without using any extra Resource for best : Big O(n)
     '0' => 'not exist','1' => 'not exist','2' => 'not exist',
     '7' => 'not exist','8' => 'not exist','9' => 'not exist',
   ];
-
+   //why I but 'not exist' above? for didnot use extra Resource for best : Big O(n) 
 
  echo $CreditCard[$firstNumber];
  
-
-
-
 ?>
